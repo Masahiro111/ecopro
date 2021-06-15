ac11ba0d657e6ac93ee94e9cea1167883ec03f98
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-06-15 23:27:58 +0900

    111

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index()
     {
         return view('frontend.index');
+    }
+
+    public function UserLogout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+        return view('frontend.frofile.user_profile', compact('user'));
     }
 }
