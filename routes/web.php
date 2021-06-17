7a671bf7ae997ab268de0b0fd6ad162218fcf7d5
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-06-18 02:30:54 +0900

    115

--- routes/web.php
+++ routes/web.php
@@ -60,10 +60,12 @@ Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function 
     return view('dashboard', compact('user'));
 })->name('dashboard');
 
-Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::middleware(['auth:sanctum,web', 'verified'])->get('/', [IndexController::class, 'index'])->name('index');
 
 Route::get('/user/logout', [IndexController::class, 'UserLogout'])->name('user.logout');
 
 Route::get('/user/profile', [IndexController::class, 'UserProfile'])->name('user.profile');
 
 Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])->name('user.profile.store');
+
+Route::get('/user/change/password', [IndexController::class, 'UserChangePassword'])->name('change.password');
